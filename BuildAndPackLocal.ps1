d7d9ea7aa7e9d6d5b06e0d32d687fa8ec8aad2b7
Hinni <michael@hinni.solutions>
2019-02-08 20:52:35 +0100

    Added possibility to build and pack nuget locally

---
+++ BuildAndPackLocal.ps1
@@ -0,0 +1,5 @@
+# Publish as self-contained
+dotnet publish .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --self-contained --output .\obj\publish\
+
+# Pack all that stuff
+dotnet pack .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --version-suffix "dev-001"
