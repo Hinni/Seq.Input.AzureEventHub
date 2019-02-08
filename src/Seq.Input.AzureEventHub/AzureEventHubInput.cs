13b1fef1556d5ecb6104245cab58c722dff8d619
Hinni <michael@hinni.solutions>
2019-02-08 19:58:14 +0100

    Added InputTypes and HelpTexts for each SeqAppSetting

--- src/Seq.Input.AzureEventHub/AzureEventHubInput.cs
+++ src/Seq.Input.AzureEventHub/AzureEventHubInput.cs
@@ -16,31 +16,36 @@ namespace Seq.Input.AzureEventHub
         [SeqAppSetting(
             DisplayName = "Event Hubs connection string",
             IsOptional = false,
-            HelpText = "")]
+            InputType = SettingInputType.Password,
+            HelpText = "Connection string for the Event Hub to receive from.")]
         public string EventHubConnectionString { get; set; }
 
         [SeqAppSetting(
             DisplayName = "Event Hub path/name",
             IsOptional = false,
-            HelpText = "")]
+            InputType = SettingInputType.Text,
+            HelpText = "The name of the EventHub.")]
         public string EventHubName { get; set; }
 
         [SeqAppSetting(
-            DisplayName = "Comsumer group name",
+            DisplayName = "Consumer group name",
             IsOptional = false,
-            HelpText = "The used consumer group name. The default is `$Default`.")]
+            InputType = SettingInputType.Text,
+            HelpText = "The name of the consumer group within the Event Hub. The default is `$Default`.")]
         public string ConsumerGroupName { get; set; } = PartitionReceiver.DefaultConsumerGroupName;
 
         [SeqAppSetting(
             DisplayName = "Storage account connection string",
             IsOptional = false,
-            HelpText = "")]
+            InputType = SettingInputType.Password,
+            HelpText = "Connection string to Azure Storage account used for leases and checkpointing.")]
         public string StorageConnectionString { get; set; }
 
         [SeqAppSetting(
             DisplayName = "Storage account container name",
             IsOptional = false,
-            HelpText = "")]
+            InputType = SettingInputType.Text,
+            HelpText = "Azure Storage container name for use by built-in lease and checkpoint manager.")]
         public string StorageContainerName { get; set; }
 
         public void Start(TextWriter inputWriter)
