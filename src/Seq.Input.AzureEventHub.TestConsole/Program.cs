2215dcfa109c5d45088c4d8d27cb0b8d5fe4db4b
Hinni <michael@hinni.solutions>
2019-02-09 13:39:41 +0100

    Reworked on TestConsole for better testing

--- src/Seq.Input.AzureEventHub.TestConsole/Program.cs
+++ src/Seq.Input.AzureEventHub.TestConsole/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Azure.EventHubs;
 using Serilog;
+using Serilog.Formatting.Compact;
 using System;
-using System.Text;
 
 namespace Seq.Input.AzureEventHub.TestConsole
 {
@@ -9,46 +9,54 @@ namespace Seq.Input.AzureEventHub.TestConsole
     {
         static void Main(string[] args)
         {
-            // Setup local logger
-            var log = new LoggerConfiguration()
-                .WriteTo.ColoredConsole()
+            // I want to see internal serilog errors, if Sinks don't work as expected
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+            // Load configuration from config
+            var appSettings = new AppSettings();
+
+            // Prepare Event Hub connection string and create Client instance
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(appSettings.EventHubConnectionString)
+            {
+                EntityPath = appSettings.EventHubName
+            };
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            // Setup local logger with two sinks
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole() // Writes local log to console
+                .WriteTo.AzureEventHub(new CompactJsonFormatter(), eventHubClient) // Writes local log to event hub for demo entries
                 .MinimumLevel.Verbose()
                 .CreateLogger();
 
-            // Load configuration
-            var appSettings = new AppSettings();
+            Log.Logger.Information("Write some test data to event hub");
 
-            // Prepare Event Hub connection string
-            var eventHubConnectionString = appSettings.EventHubConnectionString;
-            var eventHubName = appSettings.EventHubName;
-            var connectionStringBuilder = new EventHubsConnectionStringBuilder(eventHubConnectionString)
-            {
-                EntityPath = eventHubName
-            };
+            // Send test data with different levels
+            Log.Logger.Verbose("Test Event");
+            Log.Logger.Debug("Test Event");
+            Log.Logger.Information("Test Event");
+            Log.Logger.Warning("Test Event");
+            Log.Logger.Error("Test Event");
+            Log.Logger.Fatal("Test Event");
 
-            Console.WriteLine("Write some test data to event hub");
+            Log.Logger.Information("Read this data");
 
-            // Send data
-            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
-            eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes("{\"@t\":\"2019-02-09T03:44:58.8532799Z\",\"@mt\":\"Hello, {User}\",\"User\":\"nblumhardt\"}")));
-            eventHubClient.Close();
-
-            Console.WriteLine("Read this data");
-
-            // Setup listener
-            var listener = new AzureEventHubListener(Console.Out, log, true,
-                eventHubConnectionString,
-                eventHubName,
+            // Setup listener (only if not running on Seq already)
+            var listener = new AzureEventHubListener(Console.Out, Log.Logger, false,
+                appSettings.EventHubConnectionString,
+                appSettings.EventHubName,
                 appSettings.ConsumerGroupName,
                 appSettings.StorageConnectionString,
                 appSettings.StorageContainerName);
 
-            Console.WriteLine("Press any key to shutting down");
+            Log.Logger.Information("Press any key to shutting down");
             Console.ReadLine();
-            Console.WriteLine("Please wait - shutting down...");
+            Log.Logger.Warning("Please wait - shutting down...");
 
             // Clean up
-            listener.Stop();
+            listener.Stop(); // This requires 60 seconds!
+            Log.CloseAndFlush();
+            eventHubClient.Close();
         }
     }
 }
