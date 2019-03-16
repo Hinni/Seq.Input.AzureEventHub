23e1b23a8fd334d53864b8d429d72bf8edb59374
Hinni <michael@hinni.solutions>
2019-03-16 13:24:20 +0100

    Small fixes

--- BuildAndPackLocal.ps1
+++ BuildAndPackLocal.ps1
@@ -2,4 +2,4 @@
 dotnet publish .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --self-contained --output .\obj\publish\
 
 # Pack all that stuff
-dotnet pack .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --version-suffix "dev-001"
+dotnet pack .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --version-suffix "pre-20190316-001"
