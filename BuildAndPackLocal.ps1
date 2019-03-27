4ed138ff7aebeefc30e6e4171e4e01224e86dcbf
Hinni <michael@hinni.solutions>
2019-03-27 22:16:21 +0100

    Support multiple frameworks

--- BuildAndPackLocal.ps1
+++ BuildAndPackLocal.ps1
@@ -1,5 +1,6 @@
 # Publish as self-contained
-dotnet publish .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --self-contained --output .\obj\publish\
+dotnet publish .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --framework netstandard2.0 --self-contained --output .\obj\publish\netstandard2.0
+dotnet publish .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --framework net462 --self-contained --output .\obj\publish\net462
 
 # Pack all that stuff
-dotnet pack .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --version-suffix "pre-20190316-001"
+dotnet pack .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --version-suffix "pre-20190327-001"
