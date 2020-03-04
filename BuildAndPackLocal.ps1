ef9e25dd9981863046d029936d5e19a60466e7c4
Hinni <michael@hinni.solutions>
2020-03-04 23:48:30 +0100

    Switch to 3.1 and latest vm image

--- BuildAndPackLocal.ps1
+++
@@ -1,5 +0,0 @@
-# Publish as self-contained
-dotnet publish .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --framework netstandard2.0 --self-contained --output .\obj\publish\netstandard2.0
-
-# Pack all that stuff
-dotnet pack .\src\Seq.Input.AzureEventHub\Seq.Input.AzureEventHub.csproj --version-suffix "pre-20190415-001"
